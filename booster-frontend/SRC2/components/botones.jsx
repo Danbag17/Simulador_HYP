62887c4cc53b23d844fa9bf63d48b38f513869d4
Danbag17 <danibany18@gmail.com>
2026-05-13 19:31:19 +0200

    fix: actualizar la URL en la función enviarComando para redirigir correctamente al backend

--- booster-frontend/SRC2/components/botones.jsx
+++ booster-frontend/SRC2/components/botones.jsx
@@ -26,7 +26,7 @@ export function Botones() {
              if (comando === "START") {
                 bodyData.payload = { mass: Number(masa) };
             }
-            const respuesta = await fetch("http://localhost:8001/api/command", {
+            const respuesta = await fetch(" https://prideful-reroute-scrooge.ngrok-free.dev -> http://localhost:8001", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(bodyData)

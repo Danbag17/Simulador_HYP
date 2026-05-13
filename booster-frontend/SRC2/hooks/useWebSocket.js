4def5a1a09f4ced7fb7f882fc3402505a6497f75
Danbag17 <danibany18@gmail.com>
2026-05-14 00:25:08 +0200

    fix: actualizar la URL de backend en useWebSocket.js

--- booster-frontend/SRC2/hooks/useWebSocket.js
+++ booster-frontend/SRC2/hooks/useWebSocket.js
@@ -2,7 +2,7 @@ import { useEffect  } from "react";
 import { useBooster } from "../context/boosterContext.jsx";
 
 //const URL_Backend = "ws://localhost:5001/backend/stream";
-const URL_Backend = "wss://prideful-reroute-scrooge.ngrok-free.dev/backend/stream";
+const URL_Backend = "https://circumstances-invention-warned-reproduction.trycloudflare.com";
 
 const MAX_PUNTOS = 40
 

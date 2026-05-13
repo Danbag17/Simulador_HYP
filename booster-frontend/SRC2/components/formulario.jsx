63569fed1d3f0d1b990601364cb1a03d9559b57f
Danbag17 <danibany18@gmail.com>
2026-05-14 01:08:27 +0200

    fix: actualizar la URL de la API en la función calcularFrenado

--- booster-frontend/SRC2/components/formulario.jsx
+++ booster-frontend/SRC2/components/formulario.jsx
@@ -26,7 +26,7 @@ export function Calculadora() {
         setCargando(true);
 
         try {
-            const res = await fetch(`http://localhost:8001/api/calculate?m=${masaNum}&d=${distanciaNum}`);
+            const res = await fetch(`/api/calculate?m=${masaNum}&d=${distanciaNum}`);
 
             if (!res.ok) {
         
